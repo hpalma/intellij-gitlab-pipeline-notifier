7c0e3be93268040aec859b28df053330b8704aa6
Hugo Palma <hugo.m.palma@gmail.com>
2026-08-28 12:08:16 +0100

    use ProjectActivity

--- src/main/java/org/hugopalma/gitlabpipelinenotifier/startup/PipelineNotifierStartup.java
+++ src/main/java/org/hugopalma/gitlabpipelinenotifier/startup/PipelineNotifierStartup.java
@@ -1,23 +1,21 @@
 package org.hugopalma.gitlabpipelinenotifier.startup;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.startup.ProjectActivity;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
 import org.hugopalma.gitlabpipelinenotifier.watch.PipelinePoller;
-import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 /**
  * Starts polling when a project opens.
- *
- * <p>Uses {@link StartupActivity} rather than the newer {@code ProjectActivity}: the latter is a
- * Kotlin suspending interface whose Java signature takes a {@code Continuation}, which is not worth
- * the noise here. {@code StartupActivity} is marked obsolete but is neither deprecated for removal
- * nor internal.
  */
-@SuppressWarnings("UnstableApiUsage")
-public class PipelineNotifierStartup implements StartupActivity {
+public class PipelineNotifierStartup implements ProjectActivity {
 
     @Override
-    public void runActivity(@NotNull Project project) {
+    public @Nullable Object execute(@NonNull Project project, @NonNull Continuation<? super Unit> continuation) {
         PipelinePoller.getInstance(project).start();
+        return null;
     }
 }
